695dbc80156a526c34782e32f5374fc2c5a2bd73
A. Nelson <65572920+anelsonsl@users.noreply.github.com>
2020-05-29 20:16:41 +0200

    Update password.py

--- password.py
+++ password.py
@@ -3,27 +3,43 @@ import random
 letrasg=['Q','E','R','P','A','D','F','G','B','N','M']
 letrasp=['q','e','r','p','a','d','f','g','b','n','m']
 simbolos=['!','§','$','%','&','/','(',')','=','+','*','#','-','_','.',':',',',';','<','>']
-numeros=['1','2','3','4','5','6','7','8','9','0']
+numeros=['1','2','3','4','5','6','7','8','9']
 
 
 
 quantaspass= int(input('Quantas passwords para gerar:'))
 
 comprimento=int(input('Quantos Carateres:'))
+#
+# REGRAS minimu 1 Maiscula, 1 Minuscula, 1 numero, 1 Simbolo
 
-for i in range (1,quantaspass):
+for i in range (1,quantaspass+1):
 
     password=''
+    mais=0
+    minu=0
+    simb=0
+    num=0
+    
 
-    for i in range (1,comprimento):
+    for j in range (1,comprimento+1
+    ):
         tipo=random.randint(1,4)
         if tipo==1:
             password +=random.choice(letrasg)
+            mais +=1
         if tipo==2:
             password +=random.choice(letrasp)
+            minu +=1
         if tipo==3:
             password +=random.choice(simbolos)
+            simb +=1
         if tipo==4:
             password +=random.choice(numeros)
+            num +=1
+        #print(mais,minu,num,simb)
+    if mais<0 and minu<0 and simb<0 and num<0:
+        i -=1
+        password=''
 
     print(password)
